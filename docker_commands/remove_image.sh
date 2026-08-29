9cb3b49fab614adacb873c96b5e4382366205bf3
priyadarsinisahu <priyadarsinisahu27@gmail.com>
2026-08-30 00:36:00 +0530

    remove docker image

---
+++ docker_commands/remove_image.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# ----------------------------------------------------------
+# Function: Remove image (21)
+# ----------------------------------------------------------
+
+
+set -e
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
+SKY='\033[1;36m'
+NC='\033[0m'
+
+
+echo -e "\n${SKY}Docker Images${NC}"
+docker images
+
+
+read -p "Enter image name/ID: " IMAGE
+
+docker rmi "$IMAGE"
+
+echo -e "${PURPLE}Image removed${NC}"
