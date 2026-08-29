dc467f9508d4be72d36d8b64cbd320d61fba86a8
priyadarsinisahu <priyadarsinisahu27@gmail.com>
2026-08-30 00:33:53 +0530

    stop docker container

---
+++ docker_commands/stop_container.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# ----------------------------------------------------------
+# Function: Stop container (14)
+# ----------------------------------------------------------
+
+
+set -e
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
+SKY='\033[1;36m'
+NC='\033[0m'
+
+
+echo -e "${SKY}Running containers${NC}"
+docker ps
+
+
+read -p "Enter container name/ID: " CONTAINER
+
+docker stop "$CONTAINER"
+
+echo -e "$\n${YELLOW}Container stopped${NC}"
