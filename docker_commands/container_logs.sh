5ba66d42678433cc5c88d99f3966f87843fd9bd3
priyadarsinisahu <priyadarsinisahu27@gmail.com>
2026-08-30 00:21:54 +0530

    check container logs

---
+++ docker_commands/container_logs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# ----------------------------------------------------------
+# Function:  Container logs (17)
+# ----------------------------------------------------------
+
+
+set -e
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
+SKY='\033[1;36m'
+NC='\033[0m'
+
+
+echo -e "${SKY}All containers${NC}"
+docker ps -a
+
+
+read -p "Enter container name/ID: " CONTAINER
+
+echo -e "\n${SKY}Logs for container ("$CONTAINER")...... ${NC}"
+
+docker logs "$CONTAINER"
