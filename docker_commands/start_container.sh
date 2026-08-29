4a99d1585f69950ac8c4733246630c6578c3c919
priyadarsinisahu <priyadarsinisahu27@gmail.com>
2026-08-30 00:34:10 +0530

    start docker container

---
+++ docker_commands/start_container.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# ----------------------------------------------------------
+# Function: Start container (15)
+# ----------------------------------------------------------
+
+
+set -e
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
+SKY='\033[1;36m'
+NC='\033[0m'
+
+
+echo -e "${SKY}All containers${NC}"
+docker ps -a
+
+
+read -p "Enter container name/ID: " CONTAINER
+
+docker start "$CONTAINER"
+
+echo -e "$\n${PURPLE}Container started${NC}"
